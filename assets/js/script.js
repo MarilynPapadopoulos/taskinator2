1c55e7c1ecf1a55dca374d43923a771c3aeae48c
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-10-27 13:46:39 -0400

    aadded form input functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    //capture form input
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type").value;
+    //create DOM elements
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "New Item";
+    var taskInfoEl = document.createElement("div");
+    //print form input to DOM
+    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class= 'task-type'>" + taskTypeInput + "</span>"
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 }
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
