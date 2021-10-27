89d2488be819bc764b41ea0c7a57c9e757e474ad
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-10-27 08:37:36 -0400

    added ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "New Item";
+    tasksToDoEl.appendChild(listItemEl);
+}
+buttonEl.addEventListener("click", createTaskHandler);
